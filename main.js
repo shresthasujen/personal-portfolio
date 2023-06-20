008e6df83bb54f91d37c095d60ac99b635f36531
Ronesh <nakarmi.ronesh@gmail.com>
2023-06-20 13:18:38 +0545

    latest

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+window.onload = function () {
   const elements = document.querySelectorAll(".animate");
 
   let delay = 0;
-  elements.forEach(function(element) {
-    setTimeout(function() {
+  elements.forEach(function (element) {
+    setTimeout(function () {
       element.style.opacity = "1";
       element.style.transform = "translateY(0)";
     }, delay);
